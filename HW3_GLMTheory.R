4bbaa36c18f894c833a4e9455e3848e063c1fb12
paigedilmore <you@example.com>
2020-02-28 00:29:20 +0000

    finished HW - see canvas for pic

--- HW3_GLMTheory.R
+++ HW3_GLMTheory.R
@@ -1,9 +1,18 @@
 ## HW 3 ##
 
-# A
+# 1a see attached photo 
 
+# 1b
+expression(exp(theta)) %>% D("theta") #exp(theta)
+#exp(log(mu)) = mu = E[y]
 
-# B
+#1c
+expression(exp(theta) * 1) %>% D("theta") %>% D("theta") # = exp(theta)
+# Variance function
+expression(exp(theta)) %>% D("theta") %>% D("theta")
+# The mean = variance in the Poisson distribution
+
+# 2
 
 data(worldcup,package = "faraway") 
 library(dplyr) 
@@ -45,14 +54,12 @@ summary(rt_model)
 exp(coef(rt_model))
 pchisq(deviance(rt_model), rt_model$df.residual, lower.tail = FALSE) # =0
 #Although no warnings appeared about the model fitting, looking at the summary() raises some suspicion. 
-# Why: The null deviance, residual deviance, and all of the coefficients are super small; these 
-#cannot really be used to extract much information from the model. The offset(Time) seems to put the model on 
-#two different scales. 
+# Why: The null deviance and residual deviance are really large. The coefficients don't make much sense
+# when you interpret them (e.g. every increase in tackles per game = 144 more shots, really?); 
+# The offset(Time) seems to put the model on two different scales. 
 # https://stats.stackexchange.com/questions/232666/should-i-use-an-offset-for-my-poisson-glm suggests that the 
 # poisson GLM doesn't apply the offset with a log transformation automatically. 
 
-
-# This model took 5 iterations to find this result. 
 
 #d. adding control=list(maxit=2000). I believe this control argument tells the model that it can only iterate 2000 times for fitting.
 rt_model_2 <- glm(Shots ~ offset(Time) + Position + tackles90 + passes90
@@ -61,20 +68,47 @@ rt_model_2 <- glm(Shots ~ offset(Time) + Position + tackles90 + passes90
 #e. 
 summary(rt_model)
 summary(rt_model_2)
-# Compared to model 1, the summary output from model two shows extremely inflated values for the deviances
-# and coefficient estimates (compared to model 1). Moreover, Tackles per game and Passes per game are opposite signs in model 2
+# Compared to model 1, the summary output from model two shows seemingly better values for the deviances.
+# The coefficients are still confusing when interpretted.Moreover, Tackles per game and Passes per game are opposite signs in model 2
 
 # f Using model 2: calculating the DFBETAs for each observation to find outliers in the data
 # E.g. n <- c (31, 47, 18, 9, 25, 19, 62, 32)
 # e.g. pos_max <- which.max(n)
-
+install.packages("ggplot2")
+library(ggplot2)
 which.max(abs(dfbeta(rt_model_2))[2])
 head(worldcup)
 
-
-
 apply(abs(dfbeta(rt_model_2)), 2, which.max)
-worldcup[421,]
-max(abs(dfbeta(rt_model_2))
+worldcup[421,] # removing index 421, Spain Defender. He has very few shots for a large amt of playing Time and large amt of Passes
+summary(worldcup$Shots/worldcup$Passes)
+2/402
+# testing things
+# dfscatter <- worldcup[,c(4,8)]
+# head(dfscatter)
+# plot(dfscatter$Shots ~ dfscatter$tackles90, col=ifelse((dfscatter$Shots==2)
+#      & (dfscatter$tackles90 == 2.5)), "red", "black")
+# dfscatter[166,]
 
-    
+#g. remove outlier and refit
+world2 <- worldcup[-421,]
+
+rt_model_3 <- glm(Shots ~ offset(Time) + Position + tackles90 + passes90
+                  , family = poisson, data = world2, control=list(maxit=2000))
+
+summary(rt_model_3)
+exp(coef(rt_model_3))
+summary(rt_model_2)
+exp(coef(rt_model_2))
+
+# removing the outlier and refitting the model, the degrees of freedom and AICs are similiar, 
+# but the coefficient for tackles per game becomes much more pronounced.
+# It drops from .82 in model 2 to .12 in model 3. All of the signs on the coefficients
+# remain the same. 
+
+#h. interpreting passas and tackles coefficient 
+exp(coef(rt_model_3)) #passes and tackles are significant using model 3 (offset(Time) & removed index 421)
+# for tackles:  0.1280758 means that every 1 increase in tackles per game results in fractional amount of Shots. 
+# for passes: 2.032273 means that every 1 increase in passes per game results in 2.03 more Shots. This doesn't really make
+# sense intuitively (if you pass the ball, how can you take more shots), but I don't know much about soccer.
+

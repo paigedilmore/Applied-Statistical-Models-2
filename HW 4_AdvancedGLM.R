599cfa1cc28c5c74120d221b4e79509bd9e3efc8
paigedilmore <you@example.com>
2020-03-04 20:52:28 +0000

    HW4 problem 1 complete

--- HW 4_AdvancedGLM.R
+++ HW 4_AdvancedGLM.R
@@ -28,6 +28,43 @@ data$normal_pred <- predict(lm_plot_2, type="response")
 data$gamma_pred <-  predict(gamma_mod, type="response")
 data2<-data %>% gather(key = "type",value = "yield",yield, normal_pred, gamma_pred)
 ggplot(data2) +
-  geom_point(aes(yield, nitrogen, col=type), alpha=0.8)
+  geom_point(aes(nitrogen, yield, col=type), alpha=0.8)
 
-# e. 
+# e. Fit inverse Gaussian and add to previous plot
+
+inverse_mod <- glm(yield ~ (nitrogen^2),
+                 family = inverse.gaussian(link="1/mu^2"),
+                 data = cornnit[-c(21,25,37),])
+data$inv_pred <- predict(inverse_mod, type="response")
+data3<-data %>% gather(key = "type",value = "yield",yield, normal_pred, gamma_pred,inv_pred)
+ggplot(data3) +
+  geom_point(aes(nitrogen, yield, col=type), alpha=0.8)
+
+# Fits extremely similar to Gamma
+plot(data$gamma_pred, data$inv_pred, col=c(1,5))
+
+# f. 95% CI when Nitrogen = 200
+nitrogen<- 200
+nitro <- data.frame(nitrogen)
+se<- predict(gamma_mod, newdata = nitro, type="link" , se.fit = TRUE)$se.fit
+nitro$g_upper <- nitro$gamma+(1.96*se)
+nitro$g_lower <- nitro$gamma-(1.96*se)
+
+# inverse model
+nitro2 <- data.frame(nitrogen)
+se2<- predict(inverse_mod, newdata = nitro2, type="link" , se.fit = TRUE)$se.fit
+nitro2$inverse <- predict(inverse_mod, newdata = nitro2, type="link" )
+nitro2$inv_upper <- nitro2$inverse+(1.96*se2)
+nitro2$inv_lower <- nitro2$inverse-(1.96*se2)
+nitro2
+
+# Feed through inverse
+exp(nitro$g_upper)
+## 95% CI (135.3375,150.7321)
+
+(1/nitro2$inv_upper)^.5
+## 95% CI (133.61,149.27)
+
+## Very similar, but the sign switches with the inverse function switch the lower and upper bounds
+
+## Problem B

ee4a2acca6e1d32ae33ee0dd6c338676d041fdd9
paigedilmore <you@example.com>
2020-03-24 23:05:57 +0000

    problem2 hw5

--- HW5_MixedModels.R
+++ HW5_MixedModels.R
@@ -7,9 +7,9 @@ cake <- cake %>% select(-temperature)
 head(cake)
 
 #a. Fixed and random variables?
-# Recipe = Fixed
+# Recipe = Fixed level
 # Temp = Random
-# replicate = Nested? random?
+# replicate = Random
 ## RETURN
 
 # b. 
@@ -77,3 +77,61 @@ predict(mix_mod, newdata=data.frame(replicate=8, recipe="A", temp=175))
 # actual = 24, predicted = 24.588
 
 # the BLUPS follow the pattern of the box and whisker plot with replicate on x and angle on y
+
+### Problem 2###
+
+# a. train-test
+data <- Problem.2
+data
+data$ID<-as.factor(data$ID)
+set.seed(1)
+train_ind = sample(seq_len(nrow(data)),size = size)  
+train =data[train_ind,] 
+test=data[-train_ind,] 
+?seq_len
+##
+##sample = sample.split(data$num, SplitRatio = .75)
+##train = subset(data, sample == TRUE)
+##test  = subset(data, sample == FALSE)
+##
+
+# b. fitting
+install.packages("randomForest")
+library(randomForest)
+str(train)
+test[-3]
+rf <- randomForest(y ~ ., data=train)
+preds <- predict(rf, newdata=test[-3])
+resids <- (test[3]-preds)^2
+res<- cbind(test[3],preds,((test[3]-preds)^2))
+sum(res[3])/nrow(res) #MSE = 10.10093
+
+#c. Fit mixed model
+library(lmerTest)
+data
+mix_mod <- lmer(y ~ 1 + (1 | ID), data = data)
+summary(mix_mod)
+blups <- ranef(mix_mod)
+blups$ID
+library(dplyr)
+mus <- cbind(as.data.frame(data %>% group_by(ID) %>% summarise(mean_y = mean(y))),blups$ID)
+colnames<- c("ID","mean_y","blup")
+names(mus)<- colnames
+mus$sum <- mus$mean_y+mus$blup
+mus ## DF with stored blups and overall intercepts 
+# mus$sum represents the mu for y, adjusted for each group/ID factor
+mus<- mus[c(1,4)]
+
+# d. Joining blups to the test dataset
+test<-left_join(test, mus)
+head(test)
+
+# e. Store residuals
+mix_mod_res <- data.frame(residuals(mix_mod))
+mix_mod_res <- mix_mod_res[train_ind,]
+train$resids <- mix_mod_res
+# f. 
+rf2 <- randomForest(resids ~ x, data= train)
+predictions_res <- predict(rf2, newdata = test[-3])
+plot(predictions_res, test$sum)
+head(test)
